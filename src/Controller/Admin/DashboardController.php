0d39891fc71818676771c01c8b9d2c20d948ff34
jrajerison <julienrajerison5@gmail.com>
2020-01-12 10:01:19 +0300

    update view

--- src/Controller/Admin/DashboardController.php
+++ src/Controller/Admin/DashboardController.php
@@ -33,26 +33,6 @@ class DashboardController extends AbstractBaseController
      */
     public function dashboard(UserRepository $userRepository, FiainanaRepository $fiainanaRepository)
     {
-        $path = $this->getParameter('kernel.project_dir').'/public/upload/contact.yaml';
-        $data = Yaml::parseFile($path);
-        foreach ($data as $key => $val) {
-            $user = $this->manager->getRepository(User::class)->findOneBy(['username' => $key]);
-            if (!$user){
-                $user = new User();
-                $user->setRoles($user->getRoles() ?: [RoleConstant::ROLES['User']]);
-                $user->setUsername($key);
-                $user->setNom($key);
-                $user->setPrenom($val);
-                $user->setIsAbone(true);
-                $user->setBirthDate(new DateTime('now'));
-
-                if (!$user->getId()) {
-                    $user->setPassword($this->encoder->encodePassword($user, '123456'));
-                    $this->manager->persist($user);
-                }
-                $this->manager->flush();
-            }
-        }
         $users = $userRepository->findMembers();
         $fiainana = $fiainanaRepository->findAll();
 
