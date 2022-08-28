33bdc2fe44316d52a7bdd43b9b17ac6b1252f011
julkwel <julienrajerison5@gmail.com>
2022-08-28 21:18:23 +0300

    Init review design

---
+++ assets/js/front_assets.js
@@ -0,0 +1,9 @@
+require('../css/front_page.css');
+
+require("../template/Nova/assets/vendor/bootstrap/js/bootstrap.bundle.min.js");
+require("../template/Nova/assets/vendor/aos/aos.js");
+require("../template/Nova/assets/vendor/glightbox/js/glightbox.min.js");
+require("../template/Nova/assets/vendor/swiper/swiper-bundle.min.js");
+require("../template/Nova/assets/vendor/isotope-layout/isotope.pkgd.min.js");
+require("../template/Nova/assets/vendor/php-email-form/validate.js");
+require("../template/Nova/assets/js/main.js");
