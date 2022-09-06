d26e9e92e198d0761e51d8b60d979f2a431e2dc1
julkwel <julienrajerison5@gmail.com>
2022-09-06 20:32:17 +0300

    Change theming && add slides

---
+++ src/Twig/DirScannerExtension.php
@@ -0,0 +1,46 @@
+<?php
+/**
+ * @author <julienrajerison5@gmail.com>
+ */
+
+namespace App\Twig;
+
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
+use Twig\Extension\AbstractExtension;
+use Twig\TwigFunction;
+use function Clue\StreamFilter\fun;
+
+class DirScannerExtension extends AbstractExtension
+{
+    /**
+     * @var ParameterBagInterface
+     */
+    private $parameterBag;
+
+    /**
+     * @param ParameterBagInterface $parameterBag
+     */
+    public function __construct(ParameterBagInterface $parameterBag)
+    {
+        $this->parameterBag = $parameterBag;
+    }
+
+    public function getFunctions()
+    {
+        return [
+            new TwigFunction('get_slides', [$this, 'getSlideFiles']),
+        ];
+    }
+
+    /**
+     * @return array
+     */
+    public function getSlideFiles()
+    {
+        $dirs = scandir($this->parameterBag->get('kernel.root_dir').'/../public/uploads/slides/');
+
+        return array_filter($dirs, function ($item) {
+            return strpos($item, 'jpg') !== false;
+        });
+    }
+}
